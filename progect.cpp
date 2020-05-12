86b1fd8945d449e18c8a007fc1aabb7b0479f017
pressFu <gorodenko.00@mail.ru>
2020-05-12 11:09:22 +0700

    add function input

--- progect.cpp
+++ progect.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 #include<stdlib.h>
+#include "input.h"
 using namespace std;
-
+const int n = 10;
 main(){
 	setlocale(LC_ALL,"Rus");
 	int N,K;
@@ -9,8 +10,11 @@ main(){
 	char *words = new char[K];
 	cout<<"¬ведите текс: ";
 	cin>>N;
-	
-	
+	int A[n];
+	for(int i=0;i<n;i++){
+		A[i] = i;
+	}
+	input(n,A);
 	
 	
 	
