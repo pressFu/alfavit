6ea47b39d349e3833e51d491bb2837b0c7516a47
pressFu <gorodenko.00@mail.ru>
2020-06-04 12:28:37 +0700

    fixed rasp.h

--- sort.h
+++ sort.h
@@ -5,6 +5,7 @@ using namespace std;
 //=======
 //>>>>>>> master
 void sort(int N,char *p, char *words){
+	cout<<N;
 	for (int j=N-1;j>0;j--){
 		for (int i=0;i<j;i++){
     		if (strcmp(&p[words[i]], &p[words[i + 1]]) > 0){
